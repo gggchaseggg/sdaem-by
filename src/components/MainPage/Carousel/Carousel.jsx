f7f7e32d2ee1bb78d7125e2214753fc018243570
gggchaseggg <gggchaseggg@gmail.com>
2022-08-06 16:32:50 +0300

    swiper-commit-intermediate-7

--- src/components/MainPage/Carousel/Carousel.jsx
+++ src/components/MainPage/Carousel/Carousel.jsx
@@ -37,24 +37,26 @@ const Carousel = ({ className }) => {
       <span>select 1</span>
       <span>select 2</span>
       <div className={style.backgroundRectangle} />
-      <Swiper
-        modules={[Navigation]}
-        navigation
-        slidesPerView={3}
-        spaceBetween={30}
-        speed={800}
-      >
-        <SwiperSlide>Slide 1</SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
-        <SwiperSlide>Slide 3</SwiperSlide>
-        <SwiperSlide>Slide 4</SwiperSlide>
-        <SwiperSlide>Slide 5</SwiperSlide>
-        <SwiperSlide>Slide 6</SwiperSlide>
-        <SwiperSlide>Slide 7</SwiperSlide>
-        <SwiperSlide>Slide 8</SwiperSlide>
-        <SwiperSlide>Slide 9</SwiperSlide>
-        <SwiperSlide>Slide 10</SwiperSlide>
-      </Swiper>
+      <div className={style.carousel}>
+        <Swiper
+          modules={[Navigation]}
+          navigation
+          slidesPerView={3}
+          spaceBetween={30}
+          speed={800}
+        >
+          <SwiperSlide>Slide 1</SwiperSlide>
+          <SwiperSlide>Slide 2</SwiperSlide>
+          <SwiperSlide>Slide 3</SwiperSlide>
+          <SwiperSlide>Slide 4</SwiperSlide>
+          <SwiperSlide>Slide 5</SwiperSlide>
+          <SwiperSlide>Slide 6</SwiperSlide>
+          <SwiperSlide>Slide 7</SwiperSlide>
+          <SwiperSlide>Slide 8</SwiperSlide>
+          <SwiperSlide>Slide 9</SwiperSlide>
+          <SwiperSlide>Slide 10</SwiperSlide>
+        </Swiper>
+      </div>
       <div className={style.footer}>
         <h4 className={style.offersAmount}>
           341 <span className={style.plus}>+</span>
