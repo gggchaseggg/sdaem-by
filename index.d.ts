2227c379a47e5cc8a57f7262c1d34b8ae87585fa
gggchaseggg <gggchaseggg@gmail.com>
2022-08-17 14:05:10 +0300

    some-fix

--- index.d.ts
+++ index.d.ts
@@ -2,8 +2,3 @@ declare module "*.scss" {
   const content: Record<string, string>;
   export default content;
 }
-
-// declare module "*.png" {
-//   const content: string;
-//   export default content;
-// }
